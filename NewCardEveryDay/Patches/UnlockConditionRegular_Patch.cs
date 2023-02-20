66f9f3facd35fb15a7e60f063a39868f5953edfe
Str8UpWHITE64 <105523409+Str8UpWHITE64@users.noreply.github.com>
2023-02-20 13:42:59 -0700

    Add option to change day interval
    Laying ground work for adding menu option to change day interval

--- NewCardEveryDay/Patches/UnlockConditionRegular_Patch.cs
+++ NewCardEveryDay/Patches/UnlockConditionRegular_Patch.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using KitchenData;
 using System.Collections.Generic;
 
@@ -11,10 +11,19 @@ namespace KitchenNewCardEveryDay
         [HarmonyPrefix]
         public static bool ShouldProvide_PrefixPatch(UnlockConditionRegular __instance, ref bool __result, List<Unlock> candidates, HashSet<int> current_cards, UnlockRequest request)
         {
+            //set dayInterval to how often you want cards
+            int dayInterval = 1;
+            int dayOffset = __instance.DayOffset;
+
+            //if day <= DayMax or DayMax <= 0
             bool maxDay = request.Day <= __instance.DayMax || __instance.DayMax <= 0;
+            //if day >= DayMin or DayMin <= 0
             bool minDay = request.Day >= __instance.DayMin || __instance.DayMin <= 0;
+            //if (day - DayOffset) % DayInterval != 0
+            bool dayInt = (request.Day - dayOffset) % dayInterval != 0;
+            //if tier == Tier or DayMax <= 0
             bool reqTier = request.Tier == __instance.TierRequired || __instance.TierRequired <= 0;
-            __result = maxDay && minDay && reqTier;
+            __result = maxDay && minDay && dayInt && reqTier;
             return false;
         }
     }
